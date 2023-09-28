c3471bd333f4f3e28cb803d57b8b5603538772b6
Sam Anderson <samarahtanderson@gmail.com>
2023-09-28 16:29:54 -0400

    Updating name on project

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BMI Calculator
 //
-//  Created by Angela Yu on 21/08/2019.
+//  Created by Samarah Anderson on 4/23/23.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
